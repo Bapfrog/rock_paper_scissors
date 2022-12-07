c9884e266e279c66e50752a135b056f87a3ff145
Bapfrog <115760742+Bapfrog@users.noreply.github.com>
2022-12-07 10:40:20 -0500

    Complete housekeeping
    Cleaned up the comments, removed commented-out log instances, and
    basically just made it pretty for pushing to GitHub.

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 
-console.log("Hello, console. Let's Do This.");
+console.log("Hello, console. Let's (paper/scissors/)rock.");
 
 let playerName = prompt("Hello there, friend; what's your name?");
-alert( "Mucho gusto, " + playerName + "! It is time to play Rock Paper Scissors!");
+alert( "Nice to meet you, " + playerName + "! I challenge you to a game of Rock Paper Scissors!");
 
-// Write a function called getComputerChoice that randomly returns Rock, Paper, or Scissors
-
-//create the global variables for computerSelection and playerSelection (a value is optional at this point)
+//create some global variables (they don't all need a value right now)
 let computerSelection;
-let playerSelection = ""
+let playerSelection = "";
 let outcome;
 
-// - use a Math.random function to randomly select a number 1, 2, or 3. These will evaluate to Rock, Paper, and Scissors. Save this result as computerSelection and log it.
+// Write a function that randomly returns Rock, Paper, or Scissors
 function getComputerChoice() {
 
-// random # *3 plus 1. if 1, compSel = rock; 2, compSel = paper; 3, compSel = scissors
+    // random# times 3 plus 1. resulted determines if computer selects rock, paper, or scissors
     switch (Math.floor(Math.random() * 3 + 1)) {
         case 1:
             computerSelection = "rock";
@@ -25,17 +23,12 @@ function getComputerChoice() {
         case 3:
             computerSelection = "scissors";        
     }
-// log it
+    // log it
     console.log("The computer has selected: " + computerSelection);
     return computerSelection;
 }
-// invoke the function!
-//getComputerChoice();
 
-// log it again to confirm that the computerSelection assignment persists outside of the getComputerChoice() function
-//console.log("Confirming that " + computerSelection  +(", the computer's selection, persists outside of the function")); //it does
-
-// write a function to prompt for player input, convert to lower, and perform form validation (so make sure they didn't input something other than rock/paper/scissors)
+// Write a function to prompt for player input, convert same to lowercase, and validate the input
 function getSelection() {
     playerSelection = prompt("Please enter either Rock, Paper, or Scissors", "Type here");
 
@@ -55,16 +48,7 @@ function getSelection() {
     }
 }
 
-// invoke it
-// getSelection();
-// log it again now that is definitely an acceptable input
-//console.log("Player has properly inputted: " + playerSelection);
-
-// Write a function that takes 2 parameters, playerSelection and computerSelection, and then returns a string that declares the winner of a round: "You Lose! Paper beats Rock"
-// this function needs to return a result which can used later
-
-// invoke game()
-game();
+// Write a function that takes 2 parameters, playerSelection and computerSelection, and then returns a string that declares the winner of a round. It needs to return a result outcome
 
  function playRound (playerSelection, computerSelection) {
 
@@ -123,11 +107,6 @@ game();
     return outcome;
 }
 
-//does the outcome variable keep its value outside of the playRound() function?
-//console.log("confirming outcome persists outside of function: " + outcome);  //it does
-
-
-
 // game() will call the functions to get computer selection, player selection, and play a round. At the end of each round it will log the current score. After 5 rounds it will log the final score and report the winner
 function game() {
 
@@ -135,7 +114,6 @@ function game() {
     let computerScore = 0;
     let tieScore = 0;
     let scoreUpdate;
-    
 
     for (let i=0; i<5; i++) {
         getComputerChoice();
@@ -152,6 +130,7 @@ function game() {
         scoreUpdate = "Player: " + playerScore + "; Computer: " + computerScore + "; Tie: " + tieScore;
         console.log(scoreUpdate);
     }
+
     let finalScore = "The final score is " + scoreUpdate + "."
     if (playerScore > computerScore) {
         console.log("Congratulations, You Won! " + finalScore)
@@ -162,5 +141,5 @@ function game() {
     }
 }
 
-
-
+// Run it!
+game();  //It works as expected!
